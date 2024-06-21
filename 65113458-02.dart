a095e323d905eb2dccb104383417f21e102bf328
JidapaMu <penny.pennywise1105@gmail.com>
2024-06-21 13:56:41 +0700

    Add files via upload

---
+++ 65113458-02.dart
@@ -0,0 +1,12 @@
+void main() {
+  //2.Algorithm check Odd or Event
+  int number = 15;
+  if (number % 2 == 0) {
+    print('$number is even');
+  } else {
+    print('$number is odd');
+  }
+  for (int i = 1; i <= 5; i++) {
+    print('Number: $i');
+  }
+}
